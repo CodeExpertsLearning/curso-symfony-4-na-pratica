7a6faeb257c134a34c6940b154814d4559da929a
Nanderson Castro <nandokstro@gmail.com>
2017-12-17 09:55:22 -0300

    Primeiro commit. Modulo Start.

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends Controller
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index()
+    {
+        return new Response('Seja Bem Vindo ao Blog Sf4!');
+    }
+}
